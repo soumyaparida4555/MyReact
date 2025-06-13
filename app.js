e4874946319934a820567f832d60e629cf7ba07d
Soumya <soumyaparida4555@gmail.com>
2025-06-14 00:39:58 +0530

    changed

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 
 
-const Heading2=()=>(
+const Heading3=function()    //it iz basically a normal function through we create react componet usin jsx
+{
+   return <h3> Raju</h3>
+        
+   
+}
 
+const Heading2=()=>(
+     
         <h2>chintu</h2>
     
 )
 
+
+const Number=10000;
  const Heading=()=>(
     <div id="parent">
-        <Heading2/>
-  <h1 id="head">Soumya</h1>
+      
+        
+  <h1 id="head"> {Number}Soumya</h1>
+   <Heading2/>
+   {Heading3()}    
+   <Heading3/>
+   <Heading3></Heading3>
+                          
   </div>
  )
 
 
+
 const add=createRoot(document.querySelector("#chin"));
 
-add.render(<Heading/>);
+add.render(<Heading></Heading>);
